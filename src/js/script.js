abd3ad48939cde461c8944ab8ca22768cc95f85e
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-11-07 08:54:27 +0100

    ajout h2 color
    Add event listeners to change color of h2 elements

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ aMettreEnRouge.style.color= "red"
 //Bouton
 enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
 enRougeSuiteAClick.addEventListener("click", (evt)=>{enRougeSuiteAClick.style.color = "red"});
+//h2
+collH2 = document.querySelectorAll("h2")
+collH2.forEach((collH2)=>{
+    collH2.addEventListener('click', (evt)=>{
+        evt.target.style.color = "red";
+    });
+});
