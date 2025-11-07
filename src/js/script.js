d110bc9fd9d579de28547b939925582e70e607b5
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-11-07 08:41:35 +0100

    Ajout du bouton

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,6 @@
 //Changer la couleur
 aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
 aMettreEnRouge.style.color= "red"
+//Bouton
+enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougeSuiteAClick.addEventListener("click", (evt)=>{enRougeSuiteAClick.style.color = "red"});
