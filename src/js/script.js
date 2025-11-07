c551a76081a56c9e6ac048f5a98a4235749f46bb
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-11-07 09:06:30 +0100

    correction js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,14 @@
 // TODO
 //Changer la couleur
-aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
-aMettreEnRouge.style.color= "red"
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color= "red";
 //Bouton
-enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt)=>{enRougeSuiteAClick.style.color = "red"});
+// h2
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((collH2)=>{
+    collH2.addEventListener('click', (evt)=>{
+        evt.target.style.color = "red";
+    });
+});
